ed2052dab4383a6fe5f159fbe970f9bc316ff017
Anayakhan360 <anayakhan.d15@gmail.com>
2025-08-16 13:58:36 +0100

    python first class

---
+++ python/first program.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("Hi my name is Anaya and im 13 years old and my school that i go to is psi")
+print("My hobbies are basketball football and vollyball")
+print("I live in qatar")
