ef8488cf16e650aa1b861880aae4d281110b8f60
Anayakhan360 <anayakhan.d15@gmail.com>
2025-08-17 14:52:02 +0100

    python data types

--- python/first program.py
+++ python/first program.py
@@ -2,3 +2,63 @@ print("hello world")
 print("Hi my name is Anaya and im 13 years old and my school that i go to is psi")
 print("My hobbies are basketball football and vollyball")
 print("I live in qatar")
+
+# DATA TYPE-ACTIVITY-1
+my_variable = 10
+print(my_variable)
+print(type(my_variable))
+
+my_float_variable=12.4
+print(my_float_variable)
+print(type(my_float_variable))
+
+my_string="anaya"
+print(my_string)
+print(type(my_string))
+print(int(my_float_variable)) #TYPE CASTING
+
+#arithmatic-OPERATION-activity-3
+a=5
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+print(a//b)
+
+x=2
+y=3
+print(x**y)
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+#STRING-OPERATION-ACTIVITY-3
+
+first_name="Anaya"
+last_name="Khan"
+print(first_name+" "+last_name)
+
+#INDEXING OF FIRST NAME
+print(first_name[0])
+print(first_name[1])
+print(first_name[-1])
+print(first_name[0:3])
+print(first_name[0:]) #print(first_name[first_value:end_value]))
+print(first_name[:3])
+
+#activity-4 - swaping of two variables
+x=input("Enter the value of x:")
+y=input("Enter the value of y:")
+
+temp=x
+x=y
+y=temp
+print("After swapping the value of x is:",x)
+print("After swapping the value of y is:",y)
