3865618c1c4f9b028305d73426b32d140376fc67
Anayakhan360 <anayakhan.d15@gmail.com>
2026-02-01 13:44:40 +0000

    codingal.txt

--- python/flehndl.py
+++ python/flehndl.py
@@ -19,3 +19,79 @@ file_append = open('codingal.txt','a')
 file_append.write("/n File in append mode ....")
 file_append.write("Hi! I am penguin. I am 1 yr old.")
 file_append.close()
+
+
+# Activity-1
+file = open('docmnt1.txt','r')
+print("/n read in parts /n")
+print(file.read(8))
+file.close()
+
+file = open('docmnt1.txt','r')
+print("/n Read in parts /n")
+print(file.read(8))
+file.close()
+
+file = open('docmnt1.txt','a')
+file.write("hi! I am a penguin and i am 1 years old.")
+file.close()
+
+
+file1 = open('docmnt1.txt','r')
+file2 = open('codingalupdated.txt','w')
+
+for line in file1.readlines():
+
+    if not (line.startswith('coding')):
+        print(line) 
+        file2.write(line)
+file2.close()
+file1.close()
+
+#ODD LINE -Activity-4
+
+# Program to copy odd lines of one file to another
+
+# open file in read mode
+
+fn = open('Codingal.txt', 'r')
+
+# open other file in write mode
+
+fn1 = open('CodingalUpdated.txt', 'w')
+
+# read the content of the file line by line
+
+cont = fn.readlines()
+
+type(cont)
+
+for i in range(1, len(cont)+1):
+    if(i % 2 != 0):
+        fn1.write(cont[i-1])
+
+    else:
+        pass
+
+# close the file
+
+fn1.close()
+
+# open file in read mode
+
+fn1 = open('CodingalUpdated.txt', 'r')
+
+# read the content of the file
+
+cont1 = fn1.read()
+
+# print the content of the file
+
+print(cont1)
+
+# close all files
+
+fn.close()
+
+fn1.close()
+           
